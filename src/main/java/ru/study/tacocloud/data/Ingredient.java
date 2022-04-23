eb492e687c0fc975501880ecf47fac3ec890c110
Lokkill <corvolk@yandex.ru>
2022-04-23 13:11:46 +0300

    Filling in the class Ingredient

--- src/main/java/ru/study/tacocloud/data/Ingredient.java
+++ src/main/java/ru/study/tacocloud/data/Ingredient.java
@@ -1,4 +1,17 @@
 package ru.study.tacocloud.data;
 
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
 public class Ingredient {
+    private final String id;
+    private final String name;
+    private final Type type;
+
+    public static enum Type{
+        WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
 }
