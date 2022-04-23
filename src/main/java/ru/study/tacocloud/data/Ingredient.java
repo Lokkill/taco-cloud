6696eec2a4fa8c5dc240f912a31be379aa8810c2
Lokkill <corvolk@yandex.ru>
2022-04-23 13:46:46 +0300

    add new method - filter for by types

--- src/main/java/ru/study/tacocloud/data/Ingredient.java
+++ src/main/java/ru/study/tacocloud/data/Ingredient.java
@@ -4,6 +4,9 @@ package ru.study.tacocloud.data;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Data
 @RequiredArgsConstructor
 public class Ingredient {
@@ -13,5 +16,9 @@ public class Ingredient {
 
     public static enum Type{
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+    }
+
+    public static List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+        return ingredients.stream().filter(x -> x.getType().equals(type)).collect(Collectors.toList());
     }
 }
