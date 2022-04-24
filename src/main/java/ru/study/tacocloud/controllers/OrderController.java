b0731b1c6aba3cbbc615f6097e0b9e5521a83335
Lokkill <corvolk@yandex.ru>
2022-04-24 15:10:02 +0300

    add new controller OrderController

---
+++ src/main/java/ru/study/tacocloud/controllers/OrderController.java
@@ -0,0 +1,18 @@
+package ru.study.tacocloud.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.study.tacocloud.data.Order;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+    @GetMapping("/current")
+    public String orderForm(Model model){
+        model.addAttribute("order", new Order())
+    }
+}
