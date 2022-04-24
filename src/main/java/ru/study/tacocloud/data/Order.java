cbc9cec2a7a9118aed0e9e26a88c056d3bc40f9e
Lokkill <corvolk@yandex.ru>
2022-04-24 15:30:37 +0300

    add fields in Order class

--- src/main/java/ru/study/tacocloud/data/Order.java
+++ src/main/java/ru/study/tacocloud/data/Order.java
@@ -1,4 +1,17 @@
 package ru.study.tacocloud.data;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
 public class Order {
+    private String name;
+    private String street;
+    private String city;
+    private String state;
+    private String zip;
+    private String ccNumber;
+    private String ccExpiration;
+    private String ccCVV;
 }
