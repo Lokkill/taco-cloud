44c0e18030e94d9f98089233d36d63489d1b0d8b
Lokkill <corvolk@yandex.ru>
2022-04-24 15:22:56 +0300

    add post method

--- src/main/java/ru/study/tacocloud/controllers/OrderController.java
+++ src/main/java/ru/study/tacocloud/controllers/OrderController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.study.tacocloud.data.Order;
 
@@ -13,6 +14,13 @@ import ru.study.tacocloud.data.Order;
 public class OrderController {
     @GetMapping("/current")
     public String orderForm(Model model){
-        model.addAttribute("order", new Order())
+        model.addAttribute("order", new Order());
+        return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(Order order){
+        log.info("Order submitted: " + order);
+        return "redirect:/";
     }
 }
