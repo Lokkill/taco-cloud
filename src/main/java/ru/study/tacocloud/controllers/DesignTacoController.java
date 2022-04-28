00d97ec79a7c0d8d83f81da665270c26ae2cbf8b
Lokkill <corvolk@yandex.ru>
2022-04-28 19:21:31 +0300

    add initializing database schema

--- src/main/java/ru/study/tacocloud/controllers/DesignTacoController.java
+++ src/main/java/ru/study/tacocloud/controllers/DesignTacoController.java
@@ -1,40 +1,54 @@
 package ru.study.tacocloud.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import ru.study.tacocloud.data.Ingredient;
 import ru.study.tacocloud.data.Ingredient.Type;
 import ru.study.tacocloud.data.Taco;
 import ru.study.tacocloud.service.Design;
+import ru.study.tacocloud.service.IngredientRepository;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
 @Controller
 @RequestMapping("/design")
+@SessionAttributes("order")
 public class DesignTacoController {
+
+    private final IngredientRepository ingredientRepository;
+
+    @Autowired
+    public DesignTacoController(IngredientRepository ingredientRepository){
+        this.ingredientRepository = ingredientRepository;
+    }
 
     @GetMapping
     public String showDesignForm(Model model) {
-        List<Ingredient> ingredients = Arrays.asList(
-                new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP),
-                new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP),
-                new Ingredient("GBRF", "Ground Beef", Ingredient.Type.PROTEIN),
-                new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
-                new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES),
-                new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES),
-                new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE),
-                new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE),
-                new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
-                new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE)
-        );
+        List<Ingredient> ingredients = new ArrayList<>();
+        ingredientRepository.findAll().forEach(ingredients::add);
+//        List<Ingredient> ingredients = Arrays.asList(
+//                new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP),
+//                new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP),
+//                new Ingredient("GBRF", "Ground Beef", Ingredient.Type.PROTEIN),
+//                new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
+//                new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES),
+//                new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES),
+//                new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE),
+//                new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE),
+//                new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
+//                new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE)
+//        );
 
         Type[] types = Ingredient.Type.values();
 
