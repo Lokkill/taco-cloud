7f92d24df12ae780fc9644345a12e58fd56d62e7
Lokkill <corvolk@yandex.ru>
2022-05-10 19:41:30 +0300

    add registration functional

--- src/main/java/ru/study/tacocloud/security/SecurityConfig.java
+++ src/main/java/ru/study/tacocloud/security/SecurityConfig.java
@@ -1,18 +1,26 @@
 package ru.study.tacocloud.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Autowired
     private UserDetailsService userDetailsService;
@@ -33,26 +41,39 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //        ).passwordEncoder(new Pbkdf2PasswordEncoder());
 //    }
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth.userDetailsService(userDetailsService).
-                passwordEncoder(new BCryptPasswordEncoder());
-
-//        auth.ldapAuthentication()
-//                .userSearchBase("ou=people")
-//                .userSearchFilter("(uid={0})")
-//                .groupSearchBase("ou=groups")
-//                .groupSearchFilter("member={0}")
-//                .passwordCompare()
-//                .passwordEncoder(new Pbkdf2PasswordEncoder())
-//                .passwordAttribute("passcode")
-//        ;
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 //
-//        auth.ldapAuthentication()
-//                .contextSource()
-//                .root("dc=tacocloud,dc=com")
-//                .ldif("classpath:users.ldif")
-//        ;
+//        auth.userDetailsService(userDetailsService).
+//                passwordEncoder(encoder());
+//
+////        auth.ldapAuthentication()
+////                .userSearchBase("ou=people")
+////                .userSearchFilter("(uid={0})")
+////                .groupSearchBase("ou=groups")
+////                .groupSearchFilter("member={0}")
+////                .passwordCompare()
+////                .passwordEncoder(new Pbkdf2PasswordEncoder())
+////                .passwordAttribute("passcode")
+////        ;
+////
+////        auth.ldapAuthentication()
+////                .contextSource()
+////                .root("dc=tacocloud,dc=com")
+////                .ldif("classpath:users.ldif")
+////        ;
+//    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/design", "/orders")
+                .access("hasRole('ROLE_USER')")
+                .antMatchers("/", "/**")
+                .access("permitAll()")
+                .and()
+                .formLogin()
+                .loginPage("/login");
     }
 }
